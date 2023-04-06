7293e976fd652635c6ef9d9ef1066c4644a0a5b4
Joel <43355957+joelwilsonmt@users.noreply.github.com>
2023-04-06 14:39:31 -0600

    feat: all config and yarn files up to date and matching

--- amazon-ui/vite.config.js
+++ amazon-ui/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import path from "path";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/util/css-variables.css',
+          dest: ''
+        }
+      ]
+    })
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "/main.ts"),
--- apple-ui/vite.config.ts
+++ apple-ui/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import path from "path";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/util/css-variables.css',
+          dest: ''
+        }
+      ]
+    })
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "/main.ts"),
       name: "apple-ui",
     },
-  },
+  }
 });
